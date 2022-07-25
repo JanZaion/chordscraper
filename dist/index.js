e7db86e2ebeaf5a38aa0f89110cfc3452529713a
ZaionCode <zajic815@gmail.com>
2022-07-25 14:13:43 +0200

    add writeSingleResponse

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,26 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = __importDefault(require("axios"));
+const fs_1 = __importDefault(require("fs"));
+const songsterr = 'https://www.songsterr.com/a/ra/songs.json?pattern=Marley';
+const writeSingleResponse = (url, filename = 'resp') => __awaiter(void 0, void 0, void 0, function* () {
+    const res = yield axios_1.default.get(url);
+    const data = res.data;
+    console.log(typeof data);
+    const json = JSON.stringify(data, null, 4);
+    fs_1.default.writeFileSync(`./responses/${filename}.json`, json);
+});
+writeSingleResponse(songsterr);
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
 //src for apis: https://github.com/public-apis/public-apis
+import axios from 'axios';
+import fs from 'fs';
+
+const songsterr: string = 'https://www.songsterr.com/a/ra/songs.json?pattern=Marley';
+
+const writeSingleResponse = async (url: string, filename = 'resp'): Promise<void> => {
+  const res = await axios.get(url);
+  const data = res.data;
+  const json = JSON.stringify(data, null, 4);
+  fs.writeFileSync(`./responses/${filename}.json`, json);
+};
+
+writeSingleResponse(songsterr);
